546a5251a9c05dd7483eb97085463a87e35ff2ce
Chen Chao Jen <chaojen.chen@gmail.com>
2017-10-07 17:10:07 +0800

    initial version

---
+++ app/main.py
@@ -0,0 +1,24 @@
+from kivy.app import App
+from kivy.properties import StringProperty
+from kivy.properties import ListProperty
+
+from plyer import tts
+
+from random import random
+
+class MyApp(App):
+    text_input = StringProperty()
+    label_color = ListProperty()
+
+    def build(self):
+        pass
+
+    def update_text(self, text_str):
+        self.text_input = text_str
+        self.label_color = [random() for i in xrange(3)] + [1]
+
+    def speak(self):
+        tts.speak(self.text_input)
+
+if __name__ == "__main__":
+    MyApp(title='Speak').run()
